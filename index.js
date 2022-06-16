56ef3db19b20dfdc428349d014e04e14fe8ddb77
Mjakaba13 <malcolmakaba@gmail.com>
2022-06-16 08:19:16 +0000

    Up

--- index.js
+++ index.js
@@ -5,29 +5,31 @@ let displayArea = document.getElementById("display");
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
   fetch(
-    `https://restcountries.com/v2/name/${country.value.trim()}?fields=name,flag,region,capital`
+    `https://restcountries.com/v2/name/${country.value.trim()}?fields=name,flag,region,capital,population`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       if (data.length === 1) {
         data.forEach((element) => {
-          const { name, flag, region, capital } = element;
+          const { name, flag, region, capital, population } = element;
           displayArea.innerHTML = `<img style= "width: 100px" src="${flag}">
       <p>Name: ${name}</p>
       <p>Capital: ${capital}</p>
       <p>Region: ${region}</p>
+      <p>Population: ${population}</p>
       `;
         });
       } else {
         const numero = data.slice(0, 10);
         console.log(numero);
         numero.forEach((element) => {
-          const { name, flag, region, capital } = element;
+          const { name, flag, region, capital, population } = element;
           displayArea.innerHTML += `<img style= "width: 100px" src="${flag}">
       <p>Name: ${name}</p>
       <p>Capital: ${capital}</p>
       <p>Region: ${region}</p>
+      <p>Population: ${population}</p>
       `;
         });
       }
